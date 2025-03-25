046ffe74be375b3b906818043b6adad388547fcd
MitosisMan <99696985+MitosisMan@users.noreply.github.com>
2025-03-25 10:09:52 -0500

    Starter login page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,31 +1,38 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
+import EmployeeLogin from "@/components/EmployeeLogin";
 
 export const Route = createFileRoute("/")({
-	component: Index,
+  component: Index,
 });
 
 // Make index.tsx into the login screen
 function Index() {
-	// Create a request to the backend at "/" and parse the request
-	// useEffect(() => {
-	// 	async function getHome() {
-	// 		const res = await fetch("http://localhost:3000/");
-	// 		const json = await res.json();
-	// 		console.log(json["status"]);
-	// 	}
+  // Create a request to the backend at "/" and parse the request
+  // useEffect(() => {
+  // 	async function getHome() {
+  // 		const res = await fetch("http://localhost:3000/");
+  // 		const json = await res.json();
+  // 		console.log(json["status"]);
+  // 	}
 
-	// 	getHome();
-	// }, []);
+  // 	getHome();
+  // }, []);
 
-	return (
-		<div>
-			<h1>TODO: Login</h1>
-			<br />
-			<Button>
-				<Link to="/kiosk">To Kiosk</Link>
-			</Button>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <br />
+      <Button>
+        <Link to="/kiosk">Login with Google</Link>
+      </Button>
+
+	  <Button>
+        <Link to="/kiosk">Login as Guest</Link>
+      </Button>
+
+      <EmployeeLogin />
+    </div>
+  );
 }
