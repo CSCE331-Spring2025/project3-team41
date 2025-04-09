33e8e527e42f9b7cc4a9027949ed2bf03ca8b1d1
Shri G <42702641+shrig1@users.noreply.github.com>
2025-04-09 12:46:45 -0500

    Fix horrendous structure choices courtesy of Owen

--- frontend/src/components/ItemWidget.tsx
+++ frontend/src/components/ItemWidget.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from './ui/button';
 import { SquareMinus, SquarePlus } from 'lucide-react';
 import { Label } from '@radix-ui/react-label';
+import { OrderItem } from '@/routes/kiosk';
 
 interface MenuItem {
 	item: string;
@@ -12,8 +13,9 @@ interface MenuItem {
 	ingredients: string[];
 }
 
-export function ItemWidget({ item }: { item: MenuItem }) {
-    const [quantity, setQuantity] = useState<number>(1);
+export function ItemWidget({ item, o_item }: { item: MenuItem, o_item: OrderItem }) {
+    const [quantity, setQuantity] = useState<number>(o_item.quantity);
+    const [totalPrice, setTotalPrice] = useState<number>(o_item.totalPrice);
 
     return (
         <div className="flex flex-col items-center justify-center bg-gray-800 p-4 rounded-lg shadow-md w-64">
@@ -21,11 +23,26 @@ export function ItemWidget({ item }: { item: MenuItem }) {
             <p className="text-gray-400">{item.description}</p>
             <p className="text-green-500 font-semibold">${(item.price * quantity).toFixed(2)}</p>
             <div className="flex gap-2 mt-4">
-                <Button className="hover:bg-gray-600 size-7" onClick={() => setQuantity(quantity + 1)}>
+                <Button 
+                    className="hover:bg-gray-600 size-7" 
+                    onClick={() => {
+                        setQuantity(quantity + 1);
+                        setTotalPrice(totalPrice + item.price);
+                    }}
+                    >
+
                     <SquarePlus />
                 </Button>
                 <Label className="text-white text-lg">{quantity}</Label>
-                <Button className='hover:bg-gray-600 size-7' onClick={() => setQuantity(quantity - 1)} disabled={quantity <= 1}>
+                <Button 
+                    className='hover:bg-gray-600 size-7' 
+                    onClick={() => {
+                        setQuantity(quantity - 1); 
+                        setTotalPrice(totalPrice - item.price);
+                    }}
+                    disabled={quantity <= 1}
+                    >
+
                     <SquareMinus />
                 </Button>
             </div>
