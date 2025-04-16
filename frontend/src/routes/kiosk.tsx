dde73c6d08e8d821b4cc40b024b8a2f2ed501289
maaay551 <142837355+maaay551@users.noreply.github.com>
2025-04-16 00:12:56 -0500

    display temperature on screen

--- frontend/src/routes/kiosk.tsx
+++ frontend/src/routes/kiosk.tsx
@@ -24,6 +24,12 @@ interface OrderItem {
 	totalPrice: number;
 }
 
+interface WeatherData {
+	location: string;
+	temp: number;
+	condition: string;
+}
+
 export const Route = createFileRoute("/kiosk")({
 	component: RouteComponent,
 });
@@ -41,6 +47,7 @@ function RouteComponent() {
 	// console.log("Fetching menu...");
 	const [fullMenu, setFullMenu] = useState<MenuItem[]>([]);
 	let [order, setOrder] = useState<OrderItem[]>([]);
+	const [weather, setWeather] = useState<WeatherData | null>(null);
 
 	useEffect(() => {
 		async function fetchMenu() {
@@ -48,7 +55,15 @@ function RouteComponent() {
 			setFullMenu(f_menu);
 			console.log("Menu fetched:", f_menu);
 		}
+
+		async function fetchWeather() {
+			const response = await fetch(`${API_URL}/weather`);
+			const data = await response.json();
+			setWeather(data);
+		}
+
 		fetchMenu();
+		fetchWeather();
 	}, []);
 
 	//*****************Begin user token************************* */
@@ -163,129 +178,138 @@ function RouteComponent() {
 	}
 
 	return (
-		<div className="flex gap-8">
-			<div
-				className="grid grid-cols-4 gap-6"
-				style={{
-					width: "70%",
-				}}
-			>
-				{fullMenu.map((item, index) => (
-					<Button
-						key={index}
-						className="col-span-1 hover:bg-[url({fullMenu[index].image_url})]"
-						onClick={() => {
-							if (
-								!order.find(
-									(o_item) => o_item.item.item === item.item
-								)
-							) {
-								let newItem: OrderItem = {
-									item: item,
-									quantity: 1,
-									totalPrice: item.price,
-								};
-								setOrder((prevOrder) => [
-									...prevOrder,
-									newItem,
-								]);
-							} else {
-								setOrder((prevOrder) => {
-									const prev = [...prevOrder];
-									const index = order.findIndex(
-										(o_item) =>
-											o_item.item.item === item.item
-									);
-									prev[index!].quantity += 1;
-									return prev;
-								});
-							}
-						}}
-						onMouseOver={(event) => {
-							const button =
-								event.currentTarget as HTMLButtonElement;
-							if (button) {
-								// button.style.backgroundColor = "#f7a663";
-								button.style.backgroundImage = `url(${fullMenu[index].image_url})`;
-								button.style.backgroundSize = "cover";
-								button.style.backgroundPosition = "center";
-								button.style.transition =
-									"background-image 0.3s ease";
-								button.style.color = "rgba(0, 0, 0, 0)";
-							}
-							// console.log("Hovering over button:", label);
-						}}
-						onMouseOut={(event) => {
-							const button =
-								event.currentTarget as HTMLButtonElement;
-							if (button) {
-								// button.style.backgroundColor = "#f0f0f0";
-								button.style.backgroundImage = "none";
-								button.style.transition =
-									"background-image 0.3s ease";
-								button.style.color = "black";
-							}
-						}}
+		<div className="flex flex-col">
+			{weather && (
+				<div className="w-full text-white p-2 text-center mb-4">
+				{weather.location}: {weather.temp}°F
+				</div>
+			)}
+		
+
+			<div className="flex gap-8">
+				<div
+					className="grid grid-cols-4 gap-6"
+					style={{
+						width: "70%",
+					}}
+				>
+					{fullMenu.map((item, index) => (
+						<Button
+							key={index}
+							className="col-span-1 hover:bg-[url({fullMenu[index].image_url})]"
+							onClick={() => {
+								if (
+									!order.find(
+										(o_item) => o_item.item.item === item.item
+									)
+								) {
+									let newItem: OrderItem = {
+										item: item,
+										quantity: 1,
+										totalPrice: item.price,
+									};
+									setOrder((prevOrder) => [
+										...prevOrder,
+										newItem,
+									]);
+								} else {
+									setOrder((prevOrder) => {
+										const prev = [...prevOrder];
+										const index = order.findIndex(
+											(o_item) =>
+												o_item.item.item === item.item
+										);
+										prev[index!].quantity += 1;
+										return prev;
+									});
+								}
+							}}
+							onMouseOver={(event) => {
+								const button =
+									event.currentTarget as HTMLButtonElement;
+								if (button) {
+									// button.style.backgroundColor = "#f7a663";
+									button.style.backgroundImage = `url(${fullMenu[index].image_url})`;
+									button.style.backgroundSize = "cover";
+									button.style.backgroundPosition = "center";
+									button.style.transition =
+										"background-image 0.3s ease";
+									button.style.color = "rgba(0, 0, 0, 0)";
+								}
+								// console.log("Hovering over button:", label);
+							}}
+							onMouseOut={(event) => {
+								const button =
+									event.currentTarget as HTMLButtonElement;
+								if (button) {
+									// button.style.backgroundColor = "#f0f0f0";
+									button.style.backgroundImage = "none";
+									button.style.transition =
+										"background-image 0.3s ease";
+									button.style.color = "black";
+								}
+							}}
+							style={{
+								width: "100%",
+								height: "100px",
+								backgroundColor: "#f0f0f0",
+								borderRadius: "15px",
+								fontSize: "13px",
+								fontWeight: "bold",
+							}}
+						>
+							{item.item}
+						</Button>
+					))}
+				</div>
+				<div
+					className="flex flex-col gap-4"
+					style={{
+						width: "30%",
+						height: "95%",
+						backgroundColor: "#27272a",
+						borderRadius: "15px",
+						fontSize: "13px",
+						fontWeight: "bold",
+						overflowY: "auto",
+					}}
+				>
+					<Label
+						className="text-white text-2xl font-bold"
+						id="subtotal-label"
 						style={{
-							width: "100%",
-							height: "100px",
-							backgroundColor: "#f0f0f0",
-							borderRadius: "15px",
-							fontSize: "13px",
-							fontWeight: "bold",
+							textAlign: "center",
+							marginTop: "10px",
+							marginLeft: "10px",
 						}}
 					>
-						{item.item}
+						Total: $
+						{order
+							.reduce(
+								(acc, i) => (acc += i.quantity * i.item.price),
+								0
+							)
+							.toFixed(2)}
+					</Label>
+
+					{order.map((o_item, index) => ItemWidget(o_item, index))}
+
+					<Button
+						className="text-black bg-green-500 hover:bg-green-700 rounded-lg"
+						style={{
+							width: "80%",
+							height: "50px",
+							fontSize: "20px",
+							fontWeight: "bold",
+							marginLeft: "10%",
+						}}
+						onClick={() => {
+							sendOrder();
+						}}
+					>
+						Checkout
 					</Button>
-				))}
-			</div>
-			<div
-				className="flex flex-col gap-4"
-				style={{
-					width: "30%",
-					height: "95%",
-					backgroundColor: "#27272a",
-					borderRadius: "15px",
-					fontSize: "13px",
-					fontWeight: "bold",
-					overflowY: "auto",
-				}}
-			>
-				<Label
-					className="text-white text-2xl font-bold"
-					id="subtotal-label"
-					style={{
-						textAlign: "center",
-						marginTop: "10px",
-						marginLeft: "10px",
-					}}
-				>
-					Total: $
-					{order
-						.reduce(
-							(acc, i) => (acc += i.quantity * i.item.price),
-							0
-						)
-						.toFixed(2)}
-				</Label>
-
-				{order.map((o_item, index) => ItemWidget(o_item, index))}
-
-				<Button
-					className="text-black bg-green-500 hover:bg-green-700 rounded-lg"
-					style={{
-						width: "80%",
-						height: "50px",
-						fontSize: "20px",
-						fontWeight: "bold",
-						marginLeft: "10%",
-					}}
-					onClick={() => {
-						sendOrder();
-					}}
-				>
-					Checkout
-				</Button>
+				</div>
 			</div>
 		</div>
 	);
