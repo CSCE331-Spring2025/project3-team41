95c5c1524f13a01b58b28eed5b218260f1e510ec
Shri G <42702641+shrig1@users.noreply.github.com>
2025-04-21 12:57:43 -0500

    yogurt

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,7 +2,6 @@ import AppSidebar from "@/components/AppSidebar";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { Translate } from "@/components/translate";
-import { Contrast } from "@/components/contrast";
 
 export const Route = createRootRoute({
 	component: () => (
@@ -13,7 +12,6 @@ export const Route = createRootRoute({
 		</AppSidebar>
 
 		<Translate/>
-		<Contrast />
 		</div>
 	),
 });
